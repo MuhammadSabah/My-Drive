1f26e3df76db70a06cfe772d569c1dc8c78ef1b1
Muhammad Ibrahim <hama.s.ibrahim9@gmail.com>
2023-03-15 23:20:59 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="./dist/output.css" rel="stylesheet">
+    <link href="./asset/styles.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <header>
+        <nav class="relative px-24 py-4 flex justify-between items-center bg-gray-500">
+            <a class="text-2xl font-bold leading-none text-white" href="#">
+                MyDrive
+            </a>
+
+
+            <button class="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-red-500 hover:bg-red-600 text-sm text-white font-bold  rounded-xl transition duration-200" href="#">Delete</button>
+            <button class="hidden lg:inline-block py-2 lg:mr-3 px-6 bg-gray-200 hover:bg-gray-300 text-sm text-black font-bold rounded-xl transition duration-200" href="#"> Rename</button>
+            <button class="hidden lg:inline-block py-2 px-6 bg-gray-200 hover:bg-gray-300 text-sm text-black font-bold rounded-xl transition duration-200" href="#"> New Folder</button>
+        </nav>
+
+
+        </nav>
+    </header>
+    <main></main>
+</body>
+
+</html>
